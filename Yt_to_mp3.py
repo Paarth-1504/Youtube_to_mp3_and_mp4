6a85011ca8e8d84d96b82e460b3f7138559cb6fe
Paarth Sharma <70631470+Paarth-1504@users.noreply.github.com>
2021-01-29 17:21:33 +0530

    Add files via upload

---
+++ Yt_to_mp3.py
@@ -0,0 +1,46 @@
+import shutil
+import time
+
+from moviepy.editor import *
+from pytube import YouTube
+
+errors = 0
+
+
+def download_files(url):
+    global errors
+
+    try:
+        mp4 = YouTube(url).streams.get_highest_resolution().download()
+        mp3 = mp4.split(".mp4", 1)[0] + f".mp3"
+
+        video_clip = VideoFileClip(mp4)
+        audio_clip = video_clip.audio
+        audio_clip.write_audiofile(mp3)
+
+        audio_clip.close()
+        video_clip.close()
+
+        os.remove(mp4)
+        shutil.move(mp3, r"C:/Users/Paarth Sharma/Music")  # Replace this with your own output directory'
+
+    except Exception:
+        if errors < 3:
+            errors += 1
+            print(f"Something went wrong... Trying again! {errors}")
+            download_files(url)
+        else:
+            print("Could not download file.")
+
+
+def get_mp3():
+    url = input("Enter a YouTube link: ")
+    start_time = time.time()
+
+    print("Converting...")
+    download_files(url)
+
+    print(f"Time elapsed: {time.time() - start_time} seconds")
+
+
+get_mp3()
---
+++ Yt_to_mp4.py
@@ -0,0 +1,21 @@
+from pytube import YouTube
+
+#ask for the link from user
+link = input("Enter the link of YouTube video you want to download:  ")
+yt = YouTube(link)
+
+#Showing details
+print("Title: ",yt.title)
+print("Number of views: ",yt.views)
+print("Length of video: ",yt.length)
+print("Rating of video: ",yt.rating)
+
+#Getting the highest resolution possible
+print(yt.streams.filter(progressive = True))
+tag = input('Please select 18 for 360p and 22 for 720p video quality:  ')
+ys = yt.streams.get_by_itag(int(tag))
+
+#Starting download
+print("Downloading...")
+ys.download('C:/Users/Paarth Sharma/Videos')
+print("Download completed!!")
